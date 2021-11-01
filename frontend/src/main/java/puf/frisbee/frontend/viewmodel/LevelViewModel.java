335d531c6bc488946e16cc9528b8aab8465989a1
Veronika <veronika.kustermann@scandio.de>
2021-11-01 22:27:38 +0100

    Show level success dialog after time is up
    Clicking on next level reloads view with next level.

--- frontend/src/main/java/puf/frisbee/frontend/viewmodel/LevelViewModel.java
+++ frontend/src/main/java/puf/frisbee/frontend/viewmodel/LevelViewModel.java
@@ -2,8 +2,13 @@ package puf.frisbee.frontend.viewmodel;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.util.Duration;
@@ -11,38 +16,61 @@ import puf.frisbee.frontend.model.LevelModel;
 
 public class LevelViewModel {
 	private LevelModel levelModel;
-	private StringProperty countdown;
+	private StringProperty labelCountdown;
+	private StringProperty labelLevelSuccess;
+	private StringProperty buttonLevelContinueText;
 	private int second;
+	private BooleanProperty showLevelSuccessDialog;
 
 
 	public LevelViewModel(LevelModel levelModel) {
 		this.levelModel = levelModel;
-		levelModel.setCountdown(30);
-		this.countdown = new SimpleStringProperty();
+		this.labelCountdown = new SimpleStringProperty();
+		this.labelLevelSuccess = new SimpleStringProperty();
+		this.buttonLevelContinueText = new SimpleStringProperty();
+		this.showLevelSuccessDialog = new SimpleBooleanProperty(false);
 		this.startCountdown();
 	}
 
 	private void startCountdown() {
 		this.second = levelModel.getCountdown();
 		Timeline timeline = new Timeline();
-		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent actionEvent) {
-				countdown.setValue(Integer.toString(second));
-				second--;
+		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), actionEvent -> {
+			labelCountdown.setValue(Integer.toString(second));
+			second--;
 
-				if (second < 0) {
-					timeline.stop();
-					countdown.setValue("Time over");
-					// TODO: set flag for pop up if countdown is 0
-				}
+			if (second < 0) {
+				timeline.stop();
+				labelCountdown.setValue("Time over");
+				showLevelSuccessDialog.setValue(true);
 			}
 		}));
 		timeline.setCycleCount(Timeline.INDEFINITE);
 		timeline.play();
 	}
 
-	public StringProperty getCountdownProperty() {
-		return this.countdown;
+	public void continueLevel() {
+		this.levelModel.updateCurrentLevel();
+		this.showLevelSuccessDialog.setValue(false);
+	}
+
+	public int getLevel() {
+		return this.levelModel.getCurrentLevel();
+	}
+
+	public StringProperty getLabelCountdownProperty() {
+		return this.labelCountdown;
+	}
+	public StringProperty getLabelLevelSuccessProperty() {
+		this.labelLevelSuccess.setValue("Level " + this.levelModel.getCurrentLevel() + " geschafft!");
+		return this.labelLevelSuccess;
+	}
+	public StringProperty getButtonLevelContinueTextProperty() {
+		this.buttonLevelContinueText.setValue("Weiter zu Level " + this.levelModel.getCurrentLevel());
+		return this.buttonLevelContinueText;
+	}
+
+	public BooleanProperty getLevelSuccessDialogOpenProperty() {
+		return this.showLevelSuccessDialog;
 	}
 }
