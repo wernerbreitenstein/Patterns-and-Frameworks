ac82bd45d5bf23c2ca69821b3b883d9cb9d90804
Veronika <veronika.kustermann@scandio.de>
2021-11-07 17:35:23 +0100

    WiP: Extract character views

---
+++ frontend/src/main/java/puf/frisbee/frontend/view/CharacterRightView.java
@@ -0,0 +1,15 @@
+package puf.frisbee.frontend.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class CharacterRightView {
+    @FXML
+    private Label labelCharacterRight;
+
+    @FXML
+    private void handleCharacterRightClicked(MouseEvent event) {
+        labelCharacterRight.setText("Hey, ich bin Bonnie!");
+    }
+}
