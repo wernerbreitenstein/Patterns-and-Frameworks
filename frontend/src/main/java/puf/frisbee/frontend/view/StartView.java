e212d726619ee397120f752e580d8591a31bf232
Werner Breitenstein <werner.breitenstein@breitensteins.de>
2022-01-07 14:19:17 +0100

    minor changes done

--- frontend/src/main/java/puf/frisbee/frontend/view/StartView.java
+++ frontend/src/main/java/puf/frisbee/frontend/view/StartView.java
@@ -56,7 +56,6 @@ public class StartView {
 
 	@FXML
 	private void handleJoinCreateTeamButtonClicked(ActionEvent event) {
-		this.startViewModel.resetCountdown();
 		this.viewHandler.openTeamView();
 	}
 
