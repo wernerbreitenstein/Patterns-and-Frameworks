9ce605bc5c50a2ff6c7bef07d977917b079c9c15
Veronika Kustermann <mail@vk-art.de>
2022-01-17 23:16:56 +0100

    Add ready request type

--- frontend/src/main/java/puf/frisbee/frontend/network/SocketRequestType.java
+++ frontend/src/main/java/puf/frisbee/frontend/network/SocketRequestType.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public enum SocketRequestType {
     @JsonProperty("INIT")
     INIT,
+    @JsonProperty("READY")
+    READY,
     @JsonProperty("MOVE")
     MOVE,
     @JsonProperty("THROW")
