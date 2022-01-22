9de8a55c3212169f6d89dd06cad0d609d6b81fea
Veronika Kustermann <mail@vk-art.de>
2022-01-22 15:11:54 +0100

    Refactoring of network module
    No change in functionality.
    A refactoring of the switch case statement in socket client
    changed the default property value assignment though.

--- frontend/src/main/java/puf/frisbee/frontend/network/SocketRequestType.java
+++ frontend/src/main/java/puf/frisbee/frontend/network/SocketRequestType.java
@@ -2,17 +2,38 @@ package puf.frisbee.frontend.network;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Enum for the request type used for socket connection.
+ */
 public enum SocketRequestType {
+    /**
+     * Type used for socket init.
+     */
     @JsonProperty("INIT")
     INIT,
+    /**
+     * Type used for socket disconnect.
+     */
     @JsonProperty("DISCONNECT")
     DISCONNECT,
+    /**
+     * Type used for ready status.
+     */
     @JsonProperty("READY")
     READY,
+    /**
+     * Type used for game running status.
+     */
     @JsonProperty("GAME_RUNNING")
     GAME_RUNNING,
+    /**
+     * Type used for character movement.
+     */
     @JsonProperty("MOVE")
     MOVE,
+    /**
+     * Type used for frisbee throw.
+     */
     @JsonProperty("THROW")
     THROW
 }
