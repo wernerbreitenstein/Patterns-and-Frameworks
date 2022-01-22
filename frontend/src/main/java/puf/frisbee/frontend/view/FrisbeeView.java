f095ff161af7b662ef6b3874b529d5b4cc9a7e23
Veronika Kustermann <mail@vk-art.de>
2022-01-22 19:01:01 +0100

    Refactoring of frisbee view
    No change in functionality.

--- frontend/src/main/java/puf/frisbee/frontend/view/FrisbeeView.java
+++ frontend/src/main/java/puf/frisbee/frontend/view/FrisbeeView.java
@@ -6,17 +6,37 @@ import javafx.scene.input.MouseEvent;
 import puf.frisbee.frontend.viewmodel.GameViewModel;
 
 public class FrisbeeView {
+    /**
+     * Frisbee element in fxml.
+     */
     @FXML
     private ImageView frisbee;
 
+    /**
+     * The game view model instance.
+     */
     private GameViewModel gameViewModel;
 
+    /**
+     * Init method, sets the game view model instance and the initial frisbee
+     * position in the game.
+     *
+     * @param gameViewModel the game view model instance
+     */
     public void init(GameViewModel gameViewModel) {
         this.gameViewModel = gameViewModel;
-        this.frisbee.layoutXProperty().bind(gameViewModel.getFrisbeeXPositionProperty());
-        this.frisbee.layoutYProperty().bind(gameViewModel.getFrisbeeYPositionProperty());
+        this.frisbee.layoutXProperty().bind(
+                gameViewModel.getFrisbeeXPositionProperty());
+        this.frisbee.layoutYProperty().bind(
+                gameViewModel.getFrisbeeYPositionProperty());
     }
 
+    /**
+     * This method is executed on click at the frisbee. It triggeres the
+     * throw functionality in the game view model.
+     *
+     * @param event mouse event
+     */
     @FXML
     private void handleFrisbeeClicked(MouseEvent event) {
         this.gameViewModel.throwFrisbee();
