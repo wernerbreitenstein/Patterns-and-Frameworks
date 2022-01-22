38b7d7f5c8617fb1ca1b967ea559fa52f8dc8d56
Veronika Kustermann <mail@vk-art.de>
2022-01-22 20:03:36 +0100

    Refactoring of start view
    No change in functionality.

--- frontend/src/main/java/puf/frisbee/frontend/view/StartView.java
+++ frontend/src/main/java/puf/frisbee/frontend/view/StartView.java
@@ -8,57 +8,112 @@ import puf.frisbee.frontend.model.Team;
 import puf.frisbee.frontend.viewmodel.StartViewModel;
 
 public class StartView {
-	@FXML
-	private TopPanelView topPanelController;
+    /**
+     * The top panel element in fxml.
+     */
+    @FXML
+    private TopPanelView topPanelController;
 
-	@FXML
-	private BottomPanelView bottomPanelController;
+    /**
+     * The bottom panel element in fxml.
+     */
+    @FXML
+    private BottomPanelView bottomPanelController;
 
-	@FXML
-	private TableView<Team> highscoreTable;
+    /**
+     * The highscore table in fxml.
+     */
+    @FXML
+    private TableView<Team> highscoreTable;
 
-	@FXML
-	private TableColumn<Team, Integer> highscoreScore;
+    /**
+     * The highscore table column "score" in fxml.
+     */
+    @FXML
+    private TableColumn<Team, Integer> highscoreScore;
 
-	@FXML
-	private Button buttonJoinCreateTeam;
+    /**
+     * The button for join/create team in fxml.
+     */
+    @FXML
+    private Button buttonJoinCreateTeam;
 
-	@FXML
-	private Button buttonStart;
+    /**
+     * The start button in fxml.
+     */
+    @FXML
+    private Button buttonStart;
 
-	private StartViewModel startViewModel;
-	private ViewHandler viewHandler;
+    /**
+     * The start view model instance.
+     */
+    private StartViewModel startViewModel;
 
-	public void init(StartViewModel startViewModel, ViewHandler viewHandler) {
-		this.startViewModel = startViewModel;
-		this.viewHandler = viewHandler;
+    /**
+     * The view handler instance.
+     */
+    private ViewHandler viewHandler;
 
-		this.buttonJoinCreateTeam.visibleProperty().bind(this.startViewModel.getShowJoinCreateTeamButtonProperty());
-		this.buttonStart.visibleProperty().bind(this.startViewModel.getShowStartButtonProperty());
+    /**
+     * Init method. Sets the start view model instance and the view handler
+     * instance.
+     * Also initializes the top and bottom panel, and all the bindings of the
+     * fxml elements with the start view model.
+     * Fills and sorts the highscore table by score.
+     *
+     * @param startViewModel start view model instance
+     * @param viewHandler    view handler instance
+     */
+    public void init(StartViewModel startViewModel, ViewHandler viewHandler) {
+        this.startViewModel = startViewModel;
+        this.viewHandler = viewHandler;
 
-		this.topPanelController.init(startViewModel, viewHandler);
-		this.bottomPanelController.init(viewHandler);
+        this.buttonJoinCreateTeam.visibleProperty().bind(
+                this.startViewModel.getShowJoinCreateTeamButtonProperty());
+        this.buttonStart.visibleProperty().bind(
+                this.startViewModel.getShowStartButtonProperty());
 
-		this.highscoreTable.setItems(startViewModel.getHighscoreTableItems());
-		this.highscoreScore.setSortType(TableColumn.SortType.DESCENDING);
-		this.highscoreTable.getSortOrder().add(highscoreScore);
-		this.highscoreTable.sort();
-	}
+        this.topPanelController.init(startViewModel, viewHandler);
+        this.bottomPanelController.init(viewHandler);
 
-	@FXML
-	private void handleRefreshButtonClicked(ActionEvent event) {
-		this.startViewModel.refreshData();
-		this.highscoreTable.sort();
-	}
+        this.highscoreTable.setItems(startViewModel.getHighscoreTableItems());
+        this.highscoreScore.setSortType(TableColumn.SortType.DESCENDING);
+        this.highscoreTable.getSortOrder().add(highscoreScore);
+        this.highscoreTable.sort();
+    }
 
-	@FXML
-	private void handleJoinCreateTeamButtonClicked(ActionEvent event) {
-		this.viewHandler.openTeamView();
-	}
+    /**
+     * This method is called when the refresh button is clicked.
+     * It gets the newest highscore data, fills the table and sorts it by score.
+     *
+     * @param event action event
+     */
+    @FXML
+    private void handleRefreshButtonClicked(ActionEvent event) {
+        this.startViewModel.refreshData();
+        this.highscoreTable.sort();
+    }
 
-	@FXML
-	private void handleStartButtonClicked(ActionEvent event) {
-		this.startViewModel.resetCountdown();
-		this.viewHandler.openWaitingView();
-	}
+    /**
+     * This method is called when the create/join team button is clicked.
+     * It redirects to the team view.
+     *
+     * @param event action event
+     */
+    @FXML
+    private void handleJoinCreateTeamButtonClicked(ActionEvent event) {
+        this.viewHandler.openTeamView();
+    }
+
+    /**
+     * This method is called, when the start button is clicked.
+     * It resets the game timer and redirects to the waiting view.
+     *
+     * @param event action event
+     */
+    @FXML
+    private void handleStartButtonClicked(ActionEvent event) {
+        this.startViewModel.resetCountdown();
+        this.viewHandler.openWaitingView();
+    }
 }
