131a765e4a95633d80727abf0450eb3cbbd07b1c
Veronika Kustermann <mail@vk-art.de>
2022-01-22 14:18:20 +0100

    Refactoring of enums and frisbee parameter
    No change in functionality.

--- frontend/src/main/java/puf/frisbee/frontend/model/CharacterType.java
+++ frontend/src/main/java/puf/frisbee/frontend/model/CharacterType.java
@@ -4,6 +4,12 @@ package puf.frisbee.frontend.model;
  * The character types that exist - left or right.
  */
 public enum CharacterType {
+    /**
+     * The left character type.
+     */
     LEFT,
+    /**
+     * The right character type.
+     */
     RIGHT
 }
