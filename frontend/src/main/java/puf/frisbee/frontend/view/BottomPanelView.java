7259f43d1c5fad10e407c8f813a3bffad3d0e658
Veronika Kustermann <mail@vk-art.de>
2022-01-22 17:25:35 +0100

    Refactoring of bottom panel view
    No change in functionality.

--- frontend/src/main/java/puf/frisbee/frontend/view/BottomPanelView.java
+++ frontend/src/main/java/puf/frisbee/frontend/view/BottomPanelView.java
@@ -7,33 +7,71 @@ import puf.frisbee.frontend.viewmodel.WaitingViewModel;
 import puf.frisbee.frontend.viewmodel.GameViewModel;
 
 public class BottomPanelView {
-	private WaitingViewModel waitingViewModel;
-	private GameViewModel gameViewModel;
-	private ViewHandler viewHandler;
+    /**
+     * The waiting view model instance.
+     */
+    private WaitingViewModel waitingViewModel;
+    /**
+     * The game view model instance.
+     */
+    private GameViewModel gameViewModel;
+    /**
+     * The view handler instance.
+     */
+    private ViewHandler viewHandler;
 
-	public void init(WaitingViewModel waitingViewModel, ViewHandler viewHandler) {
-		this.waitingViewModel = waitingViewModel;
-		this.viewHandler = viewHandler;
-	}
+    /**
+     * Init method used in waiting view, sets waiting view model and view
+     * handler.
+     *
+     * @param waitingViewModel the waiting view model instance
+     * @param viewHandler      the view handler instance
+     */
+    public void init(WaitingViewModel waitingViewModel,
+                     ViewHandler viewHandler) {
+        this.waitingViewModel = waitingViewModel;
+        this.viewHandler = viewHandler;
+    }
 
-	public void init(GameViewModel gameViewModel, ViewHandler viewHandler) {
-		this.gameViewModel = gameViewModel;
-		this.viewHandler = viewHandler;
-	}
+    /**
+     * Init method used in game view, sets waiting view model and view
+     * handler.
+     *
+     * @param gameViewModel the game view model instance
+     * @param viewHandler   the view handler instance
+     */
+    public void init(GameViewModel gameViewModel, ViewHandler viewHandler) {
+        this.gameViewModel = gameViewModel;
+        this.viewHandler = viewHandler;
+    }
 
-	public void init(ViewHandler viewHandler) {
-		this.viewHandler = viewHandler;
-	}
+    /**
+     * Default init method, used in views without any special bottom panel
+     * handling.
+     *
+     * @param viewHandler the view handler instance
+     */
+    public void init(ViewHandler viewHandler) {
+        this.viewHandler = viewHandler;
+    }
 
-	@FXML
-	private void handleQuitGameClicked(MouseEvent event) {
-		if (gameViewModel != null) {
-			this.gameViewModel.showQuitConfirmDialog();
-		} else if (waitingViewModel != null) {
-			this.waitingViewModel.quitWaiting();
-			this.viewHandler.openStartView();
-		} else {
-			this.viewHandler.end();
-		}
-	}
+    /**
+     * Handles click on quit button, depending on where it is called.
+     * For the game view, a pause dialog is shown.
+     * For the waiting view, the user is redirected to the start view.
+     * For all other views, the application is closed.
+     *
+     * @param event the mouse event
+     */
+    @FXML
+    private void handleQuitGameClicked(MouseEvent event) {
+        if (gameViewModel != null) {
+            this.gameViewModel.showQuitConfirmDialog();
+        } else if (waitingViewModel != null) {
+            this.waitingViewModel.quitWaiting();
+            this.viewHandler.openStartView();
+        } else {
+            this.viewHandler.end();
+        }
+    }
 }
