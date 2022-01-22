c486186d98d2a237acf2b61833933b046bed6692
Veronika Kustermann <mail@vk-art.de>
2022-01-22 15:17:21 +0100

    Refactoring of some main classes
    No change in functionality.

--- frontend/src/main/java/puf/frisbee/frontend/utils/Calculations.java
+++ frontend/src/main/java/puf/frisbee/frontend/utils/Calculations.java
@@ -1,9 +1,13 @@
 package puf.frisbee.frontend.utils;
 
 public class Calculations {
+    private Calculations() {
+    }
+
     /**
-     * return true if circles touch or intersect
+     * Return true if circles touch or intersect.
      * adapted from https://stackoverflow.com/a/37563695
+     *
      * @param x1 x position circle 1
      * @param y1 y position circle 1
      * @param r1 radius circle 1
@@ -12,7 +16,8 @@ public class Calculations {
      * @param r2 radius circle 2
      * @return true if circles touch or intersect
      */
-    public static boolean circlesIntersect(double x1, double y1, double r1, double x2, double y2, double r2) {
+    public static boolean circlesIntersect(double x1, double y1, double r1,
+                                           double x2, double y2, double r2) {
         return Math.hypot(x1 - x2, y1 - y2) <= (r1 + r2);
     }
 }
