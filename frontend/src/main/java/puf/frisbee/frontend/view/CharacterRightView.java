11678a036a698d708cbf385cdcef4fe2a122ca03
Veronika Kustermann <mail@vk-art.de>
2022-01-22 18:56:23 +0100

    Refactoring of character views
    No change in functionality.

--- frontend/src/main/java/puf/frisbee/frontend/view/CharacterRightView.java
+++ frontend/src/main/java/puf/frisbee/frontend/view/CharacterRightView.java
@@ -5,14 +5,28 @@ import javafx.scene.Group;
 import puf.frisbee.frontend.viewmodel.GameViewModel;
 
 public class CharacterRightView {
+    /**
+     * Character element in fxml.
+     */
     @FXML
     private Group character;
 
+    /**
+     * Game view model instance.
+     */
     private GameViewModel gameViewModel;
 
+    /**
+     * Init method, sets the game view model and the character position in
+     * the game.
+     *
+     * @param gameViewModel the game view model instance
+     */
     public void init(GameViewModel gameViewModel) {
         this.gameViewModel = gameViewModel;
-        this.character.layoutXProperty().bind(gameViewModel.getCharacterRightXPositionProperty());
-        this.character.layoutYProperty().bind(gameViewModel.getCharacterRightYPositionProperty());
+        this.character.layoutXProperty().bind(
+                gameViewModel.getCharacterRightXPositionProperty());
+        this.character.layoutYProperty().bind(
+                gameViewModel.getCharacterRightYPositionProperty());
     }
 }
